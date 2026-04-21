20969fe7b7a192d7d193123560ed3c0b74f23c4f
keeganasmith <keeganasmith2003@gmail.com>
2026-04-21 14:39:18 -0500

    outputs

--- run_script.sh
+++ run_script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python -m tennis_pipeline.cli.run_pipeline   --input-path data/raw_data.joblib   --output-dir data  --use-elo   --use-temporal-features   --clustering-method kmeans --config-path parallel_config.json
+#python -m tennis_pipeline.cli.run_pipeline   --input-path data/raw_data.joblib   --output-dir data  --use-elo   --use-temporal-features   --clustering-method kmeans --config-path parallel_config.json
+python -m tennis_pipeline.cli.run_pipeline \
+  --input-path data/raw_data.joblib \
+  --use-elo \
+  --clustering-method kmeans \
+  --run-feature-set-experiment
